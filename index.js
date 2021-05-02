118496bb0d0c5c94773632227707e5116702388a
Mateja <mail@matejamaric.com>
2021-05-02 14:31:30 +0200

    A more verbose version of basic Node.js web server.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require('http');
 
-http.createServer((req, res) => {
+const requestHandler = (req, res) => {
+  console.log(`${req.method} ${req.url}`);
 
   res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
   res.end('<h1>lol</h1>');
+};
 
-}).listen(8080, () => console.log('Server started on port 8080.'));
+const onServerStart = () => console.log('Server started on port 8080.');
+
+const server = http.createServer(requestHandler);
+server.listen(8080, onServerStart);
