046c978ecfa3f664d9a0d423ac01075e6be170ab
Mateja <mail@matejamaric.com>
2021-05-02 20:53:15 +0200

    Started using `express-handlebars`.

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-  res.send('<h1>lol</h1>');
+  res.render('home', {
+    title: 'Home Page',
+    home: true
+  });
+});
+
+app.get('/new-post', (req, res) => {
+  res.render('new-post', {
+    title: 'Make A New Post',
+    newPost: true
+  });
 });
 
 app.listen(8080, () => console.log('Server started on port 8080.'));
