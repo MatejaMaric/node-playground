bae6d329c44701755c85fb64e9b885c907897a88
Mateja <mail@matejamaric.com>
2021-05-03 18:53:54 +0200

    Add `date` to `post` model.

--- models/post.js
+++ models/post.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
   title: String,
-  text: String
+  text: String,
+  date: {type: Date, default: Date.now}
 });
 
 const postModel = mongoose.model('post', postSchema);
