afada334b97859106c4f320e1e529358fec7a216
Mateja <mail@matejamaric.com>
2021-05-03 18:13:10 +0200

    Set `useFindAndModify` to false.

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const webRoutes = require('./routes/web');
 
 const app = express();
-mongoose.connect('mongodb://localhost/playground_db', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/playground_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
