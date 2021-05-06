7a0d256d2fabd2f1d583cea6ab0d64dcec12e45a
Mateja <mail@matejamaric.com>
2021-05-06 18:26:45 +0200

    Started using `express-session` with `connect-mongo`.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoSessionStore = require('connect-mongo');
 require('dotenv').config();
 
 const webRoutes = require('./routes/web');
 
 const app = express();
+
 mongoose.connect(process.env.DB_CONN, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false
 });
+
+app.use(session({
+  secret: process.env.APP_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: MongoSessionStore.create({
+    mongoUrl: process.env.DB_CONN
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
+  }
+}));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
